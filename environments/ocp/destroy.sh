2727ae76261d7525ef708a8cc17a82e1c89ef398
Carlos Nunez <cnunez@redhat.com>
2025-08-28 16:05:48 -0500

    [ocp] create and attach igw
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/ocp/destroy.sh
+++ environments/ocp/destroy.sh
@@ -11,6 +11,7 @@ delete_aws_ec2_key_pair() {
   key_name=$(_get_from_config '.deploy.secrets.ssh_key.name')
   test -z "$(aws ec2 describe-key-pairs --key-name "$key_name" 2>/dev/null)" && return 0
 
+  info "Deleting EC2 key pair '$key_name'"
   aws ec2 delete-key-pair --key-name "$key_name"
 }
 
@@ -41,8 +42,35 @@ delete_ec2_vpc() {
   aws ec2 delete-vpc --vpc-id "$vpc_id"
 }
 
+detach_vpc_internet_gateway() {
+  id="$(aws resourcegroupstaggingapi get-resources --tag-filters "Key=name,Values=ocp-aws-igw" |
+    jq -r '.ResourceTagMappingList[0].ResourceARN' |
+    grep -v null |
+    awk -F'/' '{print $NF}' |
+    cat)"
+  test -z "$(aws ec2 describe-internet-gateways --internet-gateway-id "$id" |
+    jq --arg id "$(_vpc_id)" \
+      -r '.InternetGateways[].Attachments[] | select(.VpcId == $id) | .VpcId' |
+    grep -v null | cat)" && return 0
+  info "Detaching Internet Gateway '$id' to VPC '$(_vpc_id)'"
+  aws ec2 detach-internet-gateway --internet-gateway-id "$id" --vpc-id "$(_vpc_id)"
+}
+
+delete_vpc_internet_gateway() {
+  id="$(aws resourcegroupstaggingapi get-resources --tag-filters "Key=name,Values=ocp-aws-igw" |
+    jq -r '.ResourceTagMappingList[0].ResourceARN' |
+    grep -v null |
+    awk -F'/' '{print $NF}' |
+    cat)"
+  test -z "$id" && return 0
+  info "Deleting internet gateway $id"
+  aws ec2 delete-internet-gateway --internet-gateway-id "$id"
+}
+
 export $(log_into_aws) || exit 1
 delete_aws_ec2_key_pair
 delete_ssh_key
+detach_vpc_internet_gateway
+delete_vpc_internet_gateway
 delete_ec2_subnets
 delete_ec2_vpc
