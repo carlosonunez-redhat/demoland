5b74ed6c52994693d4c6b22618a2a6f564c44cd2
Carlos Nunez <cnunez@redhat.com>
2025-08-29 11:29:40 -0500

    [ocp] Start using Cloudformation as described in the docs
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/ocp/destroy.sh
+++ environments/ocp/destroy.sh
@@ -20,57 +20,13 @@ delete_ssh_key() {
   rm -f "$(_get_files_from_data_dir 'id_rsa*')"
 }
 
-delete_ec2_subnets() {
-  cidr_block=$(_get_from_config '.deploy.cloud_config.aws.networking.cidr_block')
-  vpc_id="$(aws ec2 describe-vpcs |
-    jq --arg cidr "$cidr_block" -r '.Vpcs[] | select(.CidrBlock == $cidr) | .VpcId' |
-    grep -v 'null' | cat)"
-  for az in $(_all_availability_zones)
-  do
-    subnet_id="$(_vpc_subnet_from_availability_zone "$az")"
-    test -z "$subnet_id" && continue
-    info "Deleting subnet $subnet_id in VPC $vpc_id"
-    aws ec2 delete-subnet --subnet-id "$subnet_id" || return 1
-    idx=$((idx+1))
-  done
+delete_aws_vpc() {
+  info "Deleting VPC"
+  _delete_aws_resources_from_cfn_stack 'vpc' "Deleting VPC..."
 }
 
-delete_ec2_vpc() {
-  vpc_id="$(_vpc_id)"
-  test -z "$vpc_id" && return 0
-  info "Deleting AWS VPC '$vpc_id'"
-  aws ec2 delete-vpc --vpc-id "$vpc_id"
-}
-
-detach_vpc_internet_gateway() {
-  id="$(aws resourcegroupstaggingapi get-resources --tag-filters "Key=name,Values=ocp-aws-igw" |
-    jq -r '.ResourceTagMappingList[0].ResourceARN' |
-    grep -v null |
-    awk -F'/' '{print $NF}' |
-    cat)"
-  test -z "$(aws ec2 describe-internet-gateways --internet-gateway-id "$id" |
-    jq --arg id "$(_vpc_id)" \
-      -r '.InternetGateways[].Attachments[] | select(.VpcId == $id) | .VpcId' |
-    grep -v null | cat)" && return 0
-  info "Detaching Internet Gateway '$id' to VPC '$(_vpc_id)'"
-  aws ec2 detach-internet-gateway --internet-gateway-id "$id" --vpc-id "$(_vpc_id)"
-}
-
-delete_vpc_internet_gateway() {
-  id="$(aws resourcegroupstaggingapi get-resources --tag-filters "Key=name,Values=ocp-aws-igw" |
-    jq -r '.ResourceTagMappingList[0].ResourceARN' |
-    grep -v null |
-    awk -F'/' '{print $NF}' |
-    cat)"
-  test -z "$id" && return 0
-  info "Deleting internet gateway $id"
-  aws ec2 delete-internet-gateway --internet-gateway-id "$id"
-}
 
 export $(log_into_aws) || exit 1
 delete_aws_ec2_key_pair
 delete_ssh_key
-detach_vpc_internet_gateway
-delete_vpc_internet_gateway
-delete_ec2_subnets
-delete_ec2_vpc
+delete_aws_vpc
--- environments/ocp/provision.sh
+++ environments/ocp/provision.sh
@@ -32,72 +32,22 @@ upload_key_into_ec2() {
     --public-key-material "$(base64 -w 0 <<< "$pubkey")"
 }
 
-create_ec2_vpc() {
-  test -n "$(_vpc_id)" && return 0
-  cidr_block=$(_get_from_config '.deploy.cloud_config.aws.networking.cidr_block')
-  info "Creating AWS VPC with CIDR '$cidr_block'"
-  aws ec2 create-vpc --cidr-block "$cidr_block" >/dev/null
-}
-
-create_ec2_subnets() {
-  idx=0
-  cidr_block=$(_get_from_config '.deploy.cloud_config.aws.networking.cidr_block')
-  for az in $(_all_availability_zones)
-  do
-    subnet_cidr_block="$(cut -f1-2 -d '.' <<< "$cidr_block").$idx.0/24"
-    test -n "$(_vpc_subnet_from_cidr_block "$subnet_cidr_block")" && continue
-    info "Creating subnet $subnet_cidr_block in VPC $(_vpc_id) and AZ $az"
-    aws ec2 create-subnet --vpc-id "$(_vpc_id)" \
-      --cidr-block "$subnet_cidr_block" \
-      --availability-zone "$az" >/dev/null || return 1
-    idx=$((idx+1))
-  done
-}
-
-create_bootstrap_instance() {
-  ami=$(_rhcos_ami_id)
-  if test -z "$ami"
-  then
-    error "Could not find a RHCOS AMI in the region set within the config"
-    return 1
-  fi
-  key_name=$(_get_from_config '.deploy.secrets.ssh_key.name')
-  az="$(_get_from_config '.deploy.cloud_config.aws.networking.bootstrap[0]')"
-  aws ec2 run-instances --image-id "$ami" \
-    --instance-type "$(_get_from_config '.deploy.node_config.bootstrap.instance_type')" \
-    --key-name "$key_name" \
-    --monitoring false \
-    --subnet-ids "$(_vpc_subnet_from_availability_zone "$az")" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=bootstrap-${az}}]"
-  debug "AMI: $ami"
-}
-
-create_vpc_internet_gateway() {
-  info "Creating VPC internet gateway"
-  >/dev/null aws ec2 create-internet-gateway \
-    --tag-specifications "ResourceType=internet-gateway,Tags=[{Key=name,Value=ocp-aws-igw}]"
-}
-
-attach_vpc_internet_gateway_to_vpc() {
-  id="$(aws resourcegroupstaggingapi get-resources --tag-filters "Key=name,Values=ocp-aws-igw" |
-    jq -r '.ResourceTagMappingList[0].ResourceARN' |
-    grep -v null |
-    awk -F'/' '{print $NF}' |
-    cat)"
-  test -n "$(aws ec2 describe-internet-gateways --internet-gateway-id "$id" |
-    jq --arg id "$(_vpc_id)" \
-      -r '.InternetGateways[].Attachments[] | select(.VpcId == $id) | .VpcId' |
-    grep -v null | cat)" && return 0
-  info "Attaching Internet Gateway '$id' to VPC '$(_vpc_id)'"
-  aws ec2 attach-internet-gateway --internet-gateway-id "$id" --vpc-id "$(_vpc_id)"
+create_vpc() {
+  local subnet_size subnet_bits params params_json
+	subnet_size=$(_get_from_config '.deploy.cloud_config.aws.networking.subnet_size')
+	test -z "$subnet_size" && subnet_size=24
+	subnet_bits=$((32-subnet_size))
+  params=(
+    'VpcCidr' "$(_get_from_config '.deploy.cloud_config.aws.networking.cidr_block')"
+    'AvailabilityZoneCount' "$(_all_availability_zones | wc -l)"
+    'SubnetBits' "$subnet_bits"
+  )
+  params_json=$(_create_aws_cf_params_json "${params[@]}") || return 1
+  _create_aws_resources_from_cfn_stack 'vpc' "$params_json" "Creating VPC..."
 }
 
 export $(log_into_aws) || exit 1
 create_ssh_key
 load_keys_into_ssh_agent
 upload_key_into_ec2
-create_ec2_vpc
-create_ec2_subnets
-create_vpc_internet_gateway
-attach_vpc_internet_gateway_to_vpc
-#create_bootstrap_instance
+create_vpc
