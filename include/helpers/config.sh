071a1640e663efd5517300656d9a6aede124ca3b
Carlos Nunez <cnunez@redhat.com>
2025-08-29 11:29:52 -0500

    [ocp] fix: allow config values to be nil
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- include/helpers/config.sh
+++ include/helpers/config.sh
@@ -1,5 +1,5 @@
 source "$(dirname "$0")/../include/helpers/data.sh"
 _get_from_config() {
   q="$(sed -E 's;^\.;;' <<< "$1")"
-  yq -r ".$q" "$(_get_file_from_secrets_dir 'config.yaml')" | grep -Ev '^null$'
+  yq -r ".$q" "$(_get_file_from_secrets_dir 'config.yaml')" | grep -Ev '^null$' | cat
 }
