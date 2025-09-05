8032c930409acf3ce8fa27d32da01c514f7c67ac
Carlos Nunez <cnunez@redhat.com>
2025-09-05 11:16:02 -0500

    [helpers] yaml: require templates to be in include/templates
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- include/helpers/yaml.sh
+++ include/helpers/yaml.sh
@@ -8,6 +8,12 @@ as_json_string() {
   jq tostring /dev/stdin
 }
 
+_template_file() {
+  printf "%s/include/templates/%s.yaml" \
+    "$(dirname "$0")" \
+    "${1//.y*ml/}"
+}
+
 as_yaml_list() {
   local ls
   while read -r elem
@@ -18,10 +24,10 @@ as_yaml_list() {
 
 render_yaml_template() {
   local file cmd
-  file="$1"
+  file="$(_template_file "$1")"
   if ! test -f "$file"
   then
-    error "YAML file not found: $file"
+    error "YAML template not found: $file"
     return 1
   fi
   shift
@@ -37,12 +43,12 @@ render_yaml_template() {
 
 render_yaml_template_with_values_file() {
   local file values_file
-  file="$1"
+  file=$(_template_file "$1")
   values_file="$2"
   for f in "$file" "$values_file"
   do
     test -f "$f" && continue
-    error "YAML file not found: $f"
+    error "YAML file or template not found: $f"
     return 1
   done
   ytt --data-values-file "$values_file" -f "$file"
