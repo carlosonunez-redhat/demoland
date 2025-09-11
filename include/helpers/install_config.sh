0dace202384634dee1cef85cbffda1fc43f414ad
Carlos Nunez <cnunez@redhat.com>
2025-09-11 16:21:08 -0500

    [include] install_config: read from ignition files
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- include/helpers/install_config.sh
+++ include/helpers/install_config.sh
@@ -26,3 +26,20 @@ render_and_save_install_config() {
     "Couldn't generate AWS install config.") || return 1
   echo "$yaml" > "$(_config_file_in_data_dir)"
 }
+
+get_data_from_ignition_file() {
+  local ign_type
+  ign_type="$1"
+  if test -z "$ign_type"
+  then
+    error "Need to provide ignition type"
+    return 1
+  fi
+  ign_file="$(_get_file_from_data_dir "openshift-install/${ign_type}.ign")"
+  if ! test -f "$ign_file"
+  then
+    error "Couldn't find ignition file: $ign_file"
+    return 1
+  fi
+  jq -r "$2" "$ign_file"
+}
