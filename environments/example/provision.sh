60dd6e3dc82e997af324829f6370239ecdf8d13d
Carlos Nunez <cnunez@redhat.com>
2026-01-24 17:15:28 -0600

    [docs] example: Add includes to example scripts.
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

---
+++ environments/example/provision.sh
@@ -0,0 +1,17 @@
+# Provisions an environment!
+#
+# This adds some functions for working with cloud providers, the config file, and
+# other useful things.
+source "$INCLUDE_DIR/helpers/aws.sh"
+source "$INCLUDE_DIR/helpers/config.sh"
+source "$INCLUDE_DIR/helpers/data.sh"
+source "$INCLUDE_DIR/helpers/errors.sh"
+source "$INCLUDE_DIR/helpers/logging.sh"
+source "$INCLUDE_DIR/helpers/install_config.sh"
+source "$INCLUDE_DIR/helpers/yaml.sh"
+
+# If this environment has includes of its own, use the $ENVIRONMENT_INCLUDE_DIR environment
+# variable, like shown in the comment below.
+#
+# source "$ENVIRONMENT_INCLUDE_DIR/foo.sh"
+
