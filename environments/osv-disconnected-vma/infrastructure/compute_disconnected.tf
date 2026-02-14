7faeea42a15d50da2499d98e8c034659ea067823
Carlos Nunez <cnunez@redhat.com>
2026-02-13 18:30:48 -0600

    [osv] infra: one-way enable bare metal provisioning
    this saves a lot of time and money (it can take a while for metal
    instances to become available).
    
    a file sentinel is used to prevent Terraform from trying to delete the
    instances whenever 'apply' is called without the right magic.
    
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/osv-disconnected-vma/infrastructure/compute_disconnected.tf
+++ environments/osv-disconnected-vma/infrastructure/compute_disconnected.tf
@@ -11,36 +11,38 @@ module "disconnected-bastion-vm" {
 module "disconnected-artifactory-vm" {
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "artifactory"
-  instance_type = "m8a.large"
+  instance_type = local.options.cloud_config.aws.compute.instance_sizes.vm
+  ami = data.aws_ami.fedora_arm.id
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
 }
 
 module "disconnected-ocp-cp-nodes-bm" {
-  count = 3
+  count = fileexists(var.bare_metal_creation_sentinel_file) ? 3 : 0
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "ocp-cp"
-  instance_type = "m7g.metal"
+  instance_type = local.options.cloud_config.aws.compute.instance_sizes.bare_metal
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
 }
 
 module "disconnected-ocp-worker-nodes-bm" {
-  count = 3
+  count = fileexists(var.bare_metal_creation_sentinel_file) ? 3 : 0
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "ocp-cp"
-  instance_type = "m7g.metal"
+  instance_type = local.options.cloud_config.aws.compute.instance_sizes.bare_metal
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
 }
 
 module "disconnected-esx-host-bm" {
+  count = fileexists(var.bare_metal_creation_sentinel_file) ? 3 : 0
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "ocp-cp"
-  instance_type = "m7g.metal"
+  instance_type = local.options.cloud_config.aws.compute.instance_sizes.bare_metal
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
--- environments/osv-disconnected-vma/infrastructure/variables.tf
+++ environments/osv-disconnected-vma/infrastructure/variables.tf
@@ -1,3 +1,7 @@
 variable "ssh_ip" {
   description = "An IP that can SSH into the bastion"
 }
+
+variable "bare_metal_creation_sentinel_file" {
+  description = "The path to a file that, when created, provisions bare-metal AWS instances"
+}
