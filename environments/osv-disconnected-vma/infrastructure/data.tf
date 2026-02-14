c5796feb7f5b57c9a19a0b5d919c76539cea645a
Carlos Nunez <cnunez@redhat.com>
2026-02-13 18:34:03 -0600

    [osv] infra: load ssh key from file
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/osv-disconnected-vma/infrastructure/data.tf
+++ environments/osv-disconnected-vma/infrastructure/data.tf
@@ -2,3 +2,9 @@ locals {
   config = yamldecode(file("/secrets/config.yaml"))
   options = local.config.deploy
 }
+
+data "tls_public_key" "ec2_key" {
+  private_key_openssh = file("/secrets/ssh-key")
+}
+
+data "aws_region" "current" {}
