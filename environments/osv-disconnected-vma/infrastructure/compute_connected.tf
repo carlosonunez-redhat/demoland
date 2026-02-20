5b2dca7854cabf2376a75ba7151ddb57113305ca
Carlos Nunez <cnunez@redhat.com>
2026-02-19 18:37:20 -0600

    [osv] tofu: fix ami ids; use x86
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/osv-disconnected-vma/infrastructure/compute_connected.tf
+++ environments/osv-disconnected-vma/infrastructure/compute_connected.tf
@@ -1,8 +1,8 @@
 module "connected-bastion-vm" {
   source = "terraform-aws-modules/ec2-instance/aws"
-  name = "bastion"
-  ami = data.aws_ami.fedora_arm.id
-  instance_type = "m8g.large"
+  name = "bastion-connected"
+  ami = data.aws_ami.fedora_x86.id
+  instance_type = local.options.cloud_config.aws.compute.instance_sizes.vm
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.connected_network.public_subnets[0]
   vpc_security_group_ids = [ module.connected-sg-bastion.security_group_id ]
--- environments/osv-disconnected-vma/infrastructure/compute_disconnected.tf
+++ environments/osv-disconnected-vma/infrastructure/compute_disconnected.tf
@@ -1,18 +1,19 @@
 module "disconnected-bastion-vm" {
   source = "terraform-aws-modules/ec2-instance/aws"
-  name = "bastion"
-  instance_type = "m8g.large"
-  ami = data.aws_ami.fedora_arm.id
+  name = "bastion-disconnected"
+  instance_type = local.options.cloud_config.aws.compute.instance_sizes.vm
+  associate_public_ip_address = false
+  ami = data.aws_ami.fedora_x86.id
   key_name = module.ec2_key.key_pair_name
-  subnet_id = module.connected_network.private_subnets[0]
-  vpc_security_group_ids = [ module.disconnected-sg-bastion-bridge.security_group_id ]
+  subnet_id = module.disconnected_network.private_subnets[0]
+  vpc_security_group_ids = [ module.disconnected-sg-bastion.security_group_id ]
 }
 
 module "disconnected-artifactory-vm" {
   source = "terraform-aws-modules/ec2-instance/aws"
-  name = "artifactory"
+  name = "artifactory-disconnected"
   instance_type = local.options.cloud_config.aws.compute.instance_sizes.vm
-  ami = data.aws_ami.fedora_arm.id
+  ami = data.aws_ami.fedora_x86.id
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
@@ -23,6 +24,7 @@ module "disconnected-ocp-cp-nodes-bm" {
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "ocp-cp"
   instance_type = local.options.cloud_config.aws.compute.instance_sizes.bare_metal
+  ami = data.aws_ami.ipxe.id
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
@@ -33,6 +35,7 @@ module "disconnected-ocp-worker-nodes-bm" {
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "ocp-cp"
   instance_type = local.options.cloud_config.aws.compute.instance_sizes.bare_metal
+  ami = data.aws_ami.ipxe.id
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
   vpc_security_group_ids = [ module.disconnected-sg-ocp-to-artifactory.security_group_id ]
@@ -42,6 +45,7 @@ module "disconnected-esx-host-bm" {
   count = fileexists(var.bare_metal_creation_sentinel_file) ? 3 : 0
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "ocp-cp"
+  ami = data.aws_ami.ipxe.id
   instance_type = local.options.cloud_config.aws.compute.instance_sizes.bare_metal
   key_name = module.ec2_key.key_pair_name
   subnet_id = module.disconnected_network.private_subnets[0]
