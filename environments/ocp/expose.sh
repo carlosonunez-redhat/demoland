5debaac281c26722baa6f09577b213803f5c5a3e
Carlos Nunez <cnunez@redhat.com>
2026-04-01 23:19:15 -0500

    [ocp] print useful access message on expose
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/ocp/expose.sh
+++ environments/ocp/expose.sh
@@ -6,18 +6,40 @@ source "$INCLUDE_DIR/helpers/config.sh"
 source "$INCLUDE_DIR/helpers/data.sh"
 source "$INCLUDE_DIR/helpers/errors.sh"
 source "$INCLUDE_DIR/helpers/logging.sh"
+source "$INCLUDE_DIR/helpers/ocp.sh"
 source "$INCLUDE_DIR/helpers/install_config.sh"
 source "$INCLUDE_DIR/helpers/yaml.sh"
 source "$ENVIRONMENT_INCLUDE_DIR/aws.sh"
-source "$ENVIRONMENT_INCLUDE_DIR/ocp.sh"
+
+_kubeconfig_file() {
+  _get_file_from_shared_secret_dir "kubeconfigs/$(_cluster_name).kubeconfig"
+}
 
 expose_cluster_kubeconfig() {
-  f="$(_get_file_from_shared_secret_dir "kubeconfigs/ocp-aws")"
-  d="$(dirname "$f")"
-  test "$f" && return 0
-  info "Exposing cluster kubeconfig"
+  f=$(_kubeconfig_file)
+  d="$(dirname "$(_kubeconfig_file)")"
+  test -e "$f" && return 0
+  info "Saving cluster kubeconfig to '$f'"
   test -d "$d" || mkdir -p "$d"
   cp "$(_get_file_from_data_dir "openshift-install/auth/kubeconfig")" "$f"
 }
 
-expose_cluster_kubeconfig
+yay_success() {
+  local console_url
+  console_url="$(exec_oc get console cluster -o jsonpath='{.status.consoleURL}')"
+  if test -z "$console_url"
+  then
+    warning "Couldn't find console URL for cluster $(_cluster_name); check manually: \
+$(print_oc_command get console cluster -o jsonpath='{.status.consoleURL}')"
+    return 0
+  fi
+  kubeadmin_password=$(cat "$(_get_file_from_data_dir 'openshift-install/auth/kubeadmin-password')")
+  info "Your OpenShift cluster is ready! Here are your login details:
+
+URL: $console_url
+Username: kubeadmin
+Password: $kubeadmin_password"
+}
+
+expose_cluster_kubeconfig || exit 1
+yay_success
