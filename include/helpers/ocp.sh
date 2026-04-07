9b6f6a247c4abe5a2b782d83217b136f8ff6794f
Carlos Nunez <cnunez@redhat.com>
2026-04-07 11:21:45 -0500

    [fix] ocp: shorten name
    otherwise the installer will (i think????) switch to IPI mode
    and create unintended resources
    
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- include/helpers/ocp.sh
+++ include/helpers/ocp.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 _cluster_name() {
   printf "demoland-%s" "$(_get_top_level_environment_name | tr -dc '[:alnum:]')" |
-    head -c 26
+    head -c 18
 }
 
 _cluster_infra_name() {
