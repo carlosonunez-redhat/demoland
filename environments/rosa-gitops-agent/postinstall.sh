6a7742df5995fa37ccaacff57f334a67c4d61815
Carlos Nunez <cnunez@redhat.com>
2026-05-01 11:46:52 -0500

    [rosa-gitops-agent] add gitops
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

---
+++ environments/rosa-gitops-agent/postinstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Exposes data and secrets between environments during a deployment run.
+#
+# This adds some functions for working with cloud providers, the config file, and
+# other useful things.
+source "$INCLUDE_DIR/helpers/aws.sh"
+source "$INCLUDE_DIR/helpers/config.sh"
+source "$INCLUDE_DIR/helpers/data.sh"
+source "$INCLUDE_DIR/helpers/errors.sh"
+source "$INCLUDE_DIR/helpers/gitops.sh"
+source "$INCLUDE_DIR/helpers/logging.sh"
+source "$INCLUDE_DIR/helpers/install_config.sh"
+source "$INCLUDE_DIR/helpers/yaml.sh"
+
+# If this environment has includes of its own, use the $ENVIRONMENT_INCLUDE_DIR environment
+# variable, like shown in the comment below.
+#
+# source "$ENVIRONMENT_INCLUDE_DIR/foo.sh"
+source "$ENVIRONMENT_INCLUDE_DIR/rosa.sh"
+
+setup_gitops rosa-gitops-agent bootstrap-operators environment-operators
