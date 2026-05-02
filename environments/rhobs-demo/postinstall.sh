3b02045042032b353de17f142efb4ecb1d3476b9
Carlos Nunez <cnunez@redhat.com>
2026-05-02 13:55:31 -0500

    [rhobs] variable handling
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/rhobs-demo/postinstall.sh
+++ environments/rhobs-demo/postinstall.sh
@@ -20,6 +20,24 @@ create_rhobs_s3_bucket() {
   _exec_aws s3 mb "s3://$(rhobs_s3_bucket)"
 }
 
+replace_bucket_vars_in_kustomizations() {
+  local replacements_made
+  replacements_made=0
+  while read -r file
+  do
+    replacements_made=$((replacements_made+1))
+    sed "s/\$REPLACE_BUCKET\$/$(rhobs_s3_bucket)/g ; s/\$REPLACE_REGION\$/$(_exec_aws configure get region)/g" "$file"
+  done < <(grep -lr "\$REPLACE_" ./bootstrap)
+  echo "$replacements_made"
+}
+
+set -e
 create_rhobs_s3_bucket
+replacements=$(replace_bucket_vars_in_kustomizations)
+if test "$replacements" -gt 0
+then
+  info "Variables in GitOps kustomizations replaced. Commit first then perform post-install again."
+  return 0
+fi
 setup_gitops rhobs-demo bootstrap/operators bootstrap-rhobs-demo-operators
 setup_gitops rhobs-demo bootstrap/resources/rhobs rh-observability
