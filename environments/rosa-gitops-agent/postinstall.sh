dc7ca2c3412873e715181ec433e421fa783c12ab
Carlos Nunez <cnunez@redhat.com>
2026-05-02 11:59:56 -0500

    [rosa] wip: add irsa role for cluster api
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/rosa-gitops-agent/postinstall.sh
+++ environments/rosa-gitops-agent/postinstall.sh
@@ -18,5 +18,31 @@ source "$INCLUDE_DIR/helpers/yaml.sh"
 # source "$ENVIRONMENT_INCLUDE_DIR/foo.sh"
 source "$ENVIRONMENT_INCLUDE_DIR/rosa.sh"
 
+_cluster_name() {
+  oc get infrastructure cluster -o yaml |
+    yq -r '.status.platformStatus.aws.resourceTags[] | select(.key == "api.openshift.com/name") | .value'
+}
+
+create_irsa_for_cluster_api() {
+  _iam_irsa_role_exists() {
+    _exec_aws iam list --query 'Roles[*].RoleName' --output text |
+      grep -q "capa-manager-role-$(_cluster_name)"
+  }
+
+  _create_iam_irsa_role() {
+    oidc_provider=$(oc get authentication.config.openshift.io cluster -o json |
+      jq -r .spec.serviceAccountIssuer |
+      sed 's/https:\/\///')
+    oidc_provider_arn=$(_exec_aws iam list-open-id-connect-providers --query 'OpenIDConnectProviderList[*].Arn' --output text |
+      grep "$oidc_provider")
+  }
+
+  _iam_irsa_role_exists && return 0
+
+  info "Creating IRSA role for Cluster API"
+  _create_iam_irsa_role
+}
+
 setup_gitops rosa-gitops-agent bootstrap/operators environment-operators
 setup_gitops rosa-gitops-agent bootstrap/resources environment-resources
+create_irsa_for_cluster_api hcp
