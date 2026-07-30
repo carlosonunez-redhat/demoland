986c522119d50d92261ddab7ae37ac735f18fe1e
Carlos Nunez <cnunez@redhat.com>
2026-07-30 10:42:20 -0500

    [helpers] ocp: add support for multiple kubeconfigs per env
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- include/helpers/ocp.sh
+++ include/helpers/ocp.sh
@@ -41,8 +41,21 @@ _exec_oc() {
   command -- $(_oc_cmd "$1" "${@:2}")
 }
 
+_retrieve_env_kubeconfig() {
+  kubeconfigs=$(find /environment_info/kubeconfigs -mindepth 1 -type f | sort -u)
+  num_kubeconfigs=$(wc -l <<< "$kubeconfigs")
+  chosen_kubeconfig=$(head -1 <<< "$kubeconfigs")
+  if test "$num_kubeconfigs" -gt 1
+  then
+    warning "Multiple kubeconfigs written for environment $(_get_top_level_environment_name); \
+choosing '$(basename "$chosen_kubeconfig")' (use 'exec_oc_by_environment_name' to select \
+an environment)"
+  fi
+  cat "$chosen_kubeconfig"
+}
+
 exec_oc() {
-  _exec_oc "$(cat /environment_info/kubeconfig_path)" "$@"
+  _exec_oc "$(_retrieve_env_kubeconfig)" "$@"
 }
 
 exec_oc_postinstall() {
@@ -57,20 +70,20 @@ exec_oc_postinstall() {
 }
 
 print_oc_command() {
-  _oc_cmd "kubeconfigs/$(_cluster_name).kubeconfig" "$@"
+  _oc_cmd "$(_retrieve_env_kubeconfig)" "$@"
 }
 
 # saves a kubeconfig into the secret dir while also writing a reference to
 # it in the toplevel environment volume.
 expose_kubeconfig() {
   local kubeconfig_ref kubeconfig_path
-  kubeconfig_ref="/environment_info/kubeconfig_path"
+  kubeconfig_ref="/environment_info/kubeconfigs/$(_get_this_environment_name)"
+  test -d "$(dirname "$kubeconfig_ref")" || mkdir -p "$(dirname "$kubeconfig_ref")"
   if test -f "$kubeconfig_ref"
   then kubeconfig_path=$(cat "$kubeconfig_ref")
   else kubeconfig_path=$(mktemp -u "$(_get_file_from_shared_secret_dir "kubeconfigs")/XXXXXXXXXXXXXXXX.kubeconfig")
   fi
   info "Saving cluster kubeconfig to '$kubeconfig_path'"
-  test -d "$(dirname "$kubeconfig_path")" || mkdir -p "$(dirname "$kubeconfig_path")"
   echo "$1" > "$kubeconfig_path" && echo "$kubeconfig_path" > "$kubeconfig_ref"
 }
 
