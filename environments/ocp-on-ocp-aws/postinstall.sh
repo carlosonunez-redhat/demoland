e2ea1cf60b7438647c13a05faf39f323bd074de5
Carlos Nunez <cnunez@redhat.com>
2026-07-31 10:11:23 -0500

    [ocp-on-ocp] add hyperconverged to stack
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/ocp-on-ocp-aws/postinstall.sh
+++ environments/ocp-on-ocp-aws/postinstall.sh
@@ -17,6 +17,8 @@ source "$INCLUDE_DIR/helpers/yaml.sh"
 # source "$ENVIRONMENT_INCLUDE_DIR/foo.sh"
 setup_gitops ocp-on-ocp-aws bootstrap/operators operators
 setup_gitops ocp-on-ocp-aws bootstrap/resources/base resources
+info "WIP."
+exit 0
 wait_for_osv_to_become_ready
 if ! nested_ocp_cluster_created
 then
