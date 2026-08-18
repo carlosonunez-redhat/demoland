095c93d69fb7a50450e552016e60d32df8cea029
Carlos Nunez <cnunez@redhat.com>
2026-08-18 17:35:37 -0500

    [ocp-on-ocp-aws] add nncp patch
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/ocp-on-ocp-aws/postinstall.sh
+++ environments/ocp-on-ocp-aws/postinstall.sh
@@ -16,11 +16,73 @@ source "$INCLUDE_DIR/helpers/yaml.sh"
 # variable, like shown in the comment below.
 #
 # source "$ENVIRONMENT_INCLUDE_DIR/foo.sh"
+patch_nncp_for_vm_network() {
+  _find_physical_iface_members_in_brex_bridge() {
+    pod=$(_exec_oc get pod -n openshift-ovn-kubernetes \
+      --field-selector=spec.nodeName="$1" \
+      -l app=ovnkube-node \
+      -o name)
+    if test -z "$pod"
+    then
+      error "ovn pod not found on worker node '$1'"
+      return 1
+    fi
+    _exec_oc exec -n openshift-ovn-kubernetes "$pod" -- ovs-vsctl list-ports br-ex |
+      grep -Ev '^patch-' |
+      sort
+  }
+
+  _find_physical_ifaces_on_node() {
+    _exec_oc debug "node/$1" -- ip -br a | grep -E '^(en|eth)' | awk '{print $1}' | sort
+  }
+
+  _find_unclaimed_ifaces() {
+    local all claimed
+    all="$1"
+    claimed="$2"
+    comm -13 <(echo "$all") <(echo "$claimed") | sort -u
+  }
+
+  _patch_nncp_kustomization() {
+    modifications="$(cat <<-EOF
+- file: bootstrap/resources/networking/kustomization.yaml
+  variables:
+    name: "br1-$1-policy"
+    desiredState: "$1"
+EOF
+)"
+    render_kustomization_patches "$modifications"
+  }
+
+  all_available_ifaces=""
+  for node in $(_exec_oc get nodes -o name)
+  do
+    all_ifaces=$(_find_physical_ifaces_on_node "$node")
+    claimed_ifaces=$(_find_physical_iface_members_in_brex_bridge "$node")
+    available_ifaces=$(_find_unclaimed_ifaces "$all_ifaces" "$claimed_ifaces")
+    all_available_ifaces="$all_available_ifaces $available_ifaces"
+  done
+  all_available_ifaces=$(tr ' ' '\n' <<< "$all_available_ifaces" | sort -u)
+  num_unique_available_ifaces=$(wc -l <<< "$all_available_ifaces")
+  if test "$num_unique_available_ifaces" -gt 1
+  then
+    error "One or more nodes have mismatched network interface device identifiers; interfaces: $all_available_ifaces"
+    return 1
+  fi
+  patches=$(_patch_nncp_kustomization "$all_available_ifaces")
+  test "$patches" -eq 0 && return 0
+
+  info "NodeNetworkConfigurationPolicy kustomization updated. Commit and push your changes to apply."
+  return 1
+}
+
+patch_nncp_for_vm_network || return 1
 setup_gitops ocp-on-ocp-aws bootstrap/operators operators
 setup_gitops ocp-on-ocp-aws bootstrap/resources/base resources
 setup_gitops ocp-on-ocp-aws bootstrap/resources/networking networking
 info "WIP."
 exit 0
+
 wait_for_osv_to_become_ready
 if ! nested_ocp_cluster_created
 then
