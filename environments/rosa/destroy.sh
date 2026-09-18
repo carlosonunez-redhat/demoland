cdb600259ee124b61d5e0a6425a574b4ffe91499
Carlos Nunez <cnunez@redhat.com>
2026-09-18 13:18:05 -0500

    [rosa] fix: delete a specific OIDC ID
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- environments/rosa/destroy.sh
+++ environments/rosa/destroy.sh
@@ -45,7 +45,7 @@ destroy_oidc_configuration() {
 
   info "Deleting AWS OIDC config for ROSA"
   _exec_rosa delete oidc-config \
-    "$(_rosa_oidc_id)" \
+    --oidc-config-id "$(_rosa_oidc_id)" \
     --mode auto \
     --yes
 }
